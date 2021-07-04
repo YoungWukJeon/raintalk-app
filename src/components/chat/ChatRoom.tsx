70334c18f8b94612e06171f4495b8dfcd17f4eb5
YoungWukJeon <kdh10080@gmail.com>
2021-07-05 01:10:03 +0900

    채팅 방 목록 개발 중

---
+++ src/components/chat/ChatRoom.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ChatRoomProps {
+}
+
+const ChatRoom: React.FC<ChatRoomProps> = ({}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ChatRoom;
